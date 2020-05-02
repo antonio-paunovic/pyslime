d68aeeebda2685e2373abef5b8a895f263b6eb3b
antonio.paunovic <antonio.paunovic@gmail.com>
2020-05-02 17:07:29 +0200

    Fix ball reflection

--- slime.py
+++ slime.py
@@ -5,10 +5,10 @@ import numpy as np
 
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
-WHITE=(255,255,255)
-BLUE=(0,0,255)
-YELLOW=(125,0,0)
-BLACK=(0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+YELLOW = (125,0,0)
+BLACK = (0,0,0)
 
 pygame.init()
 DISPLAY= pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
@@ -57,9 +57,11 @@ class Slime(PhisicalEntity):
         DISPLAY.blit(s, tuple(self.s))
 
     def move(self, v):
+        # Slime is on the floor.
         if not (self.gravity in self.forces):
             super().move(v)
 
+        # Slime is airborne.
         if v[1] < 0: # up is negative
             self.forces.add(self.gravity)
 
@@ -88,13 +90,13 @@ class Ball(PhisicalEntity):
 
 
 slime1 = Slime(200., DISPLAY_HEIGHT-60, 60)
-ball = Ball(200., 300., 10)
+ball = Ball(200., 300., 10, 30,  30)
 entities = [slime1, ball]
 
 def rotate(vector, angle):
     """ Clock-wise rotation of vector around the angle."""
-    return np.cos(angle)*vector[0] + np.sin(angle) * vector[1], \
-        -np.sin(angle)* vector[0] + np.cos(angle) * vector[1]
+    return np.cos(angle) * vector[0] + np.sin(angle) * vector[1], \
+        -np.sin(angle) * vector[0] + np.cos(angle) * vector[1]
 
 
 while True: # main game loop
@@ -114,35 +116,79 @@ while True: # main game loop
                 slime1.move([0., -50])
 
     # Collision detection: TODO abstract this away.
+    # Bouncing of the floor.
     if ball.s[1] >= DISPLAY_HEIGHT-2*ball.r:
-        angle = np.pi
-        x,y = rotate(ball.v, angle)
-        print("floor {} {}, old {}".format(x,y, ball.v))
-        (ball.v[0], ball.v[1]) = -x, y
-        ball.update()
+        nextPosition = ball.s + ball.v * PhisicalEntity.dt
+        if nextPosition[1] > DISPLAY_HEIGHT-2*ball.r:
+            ball.s[1] = DISPLAY_HEIGHT-2*ball.r
 
-    elif ball.s[0] <= 0 :
-        angle = -np.pi / 2
-        x,y = rotate(ball.v, angle)
-        print("wall {} {}, old {}".format(x,y, ball.v))
-        ball.v[0], ball.v[1] = x, y
-        ball.update()
+        impactAngle = np.arctan2(ball.v[1], ball.v[0])
+        # print(f"Impact angle: {np.rad2deg(impactAngle)}, Out angle should be {-np.rad2deg(impactAngle)}")
+        vx, vy = rotate(ball.v, 2 * impactAngle)
+        # print("floor {} {}, old {}".format(vx,vy, ball.v))
+        ball.v[0], ball.v[1] = vx, vy
 
-    elif ball.s[0] >= DISPLAY_WIDTH:
-        angle = np.pi / 2
-        x,y = rotate(ball.v, angle)
-        print ("wall {} {}, old {}".format(x,y, ball.v))
-        ball.v[0], ball.v[1] = x, y
-        ball.update()
+        outAngle = np.arctan2(vy, vx)
+        # print(f"Out angle is {np.rad2deg(outAngle)}")
 
-    dist = np.linalg.norm((slime1.s+slime1.r) - (ball.s+ball.r), axis=0) # slime1 <-> ball euclidian distance
-    if dist <= slime1.r + ball.r:
-        d = slime1.s[1]-ball.s[1]-2*ball.r # distance from touching point to the floor
-        r = slime1.r
-        alpha = np.arcsin(d/r)
-        beta = np.pi/2 - alpha
-        x,y = rotate(ball.v, beta)
-        ball.v[0], ball.v[1] = x, y
+    # Ball hit the left wall.
+    elif ball.s[0] <= 0:
+        nextPosition = ball.s + ball.v * PhisicalEntity.dt
+        if nextPosition[0] <= 0:
+            ball.s[0] = 0 #2 * ball.r
+
+        impactAngle = np.arctan2(ball.v[1], ball.v[0])
+        # print(f"Impact angle: {np.rad2deg(impactAngle)}, Out angle should be {np.rad2deg(np.pi-impactAngle)}")
+        vx, vy = rotate(ball.v, np.pi + 2 * impactAngle)
+        # print("floor {} {}, old {}".format(vx, vy, ball.v))
+        ball.v[0], ball.v[1] = vx, vy
+
+        outAngle = np.arctan2(vy, vx)
+        # print(f"Out angle is {np.rad2deg(outAngle)}")
+
+
+    # Ball hit the right wall.
+    elif ball.s[0] >= DISPLAY_WIDTH - 2 * ball.r:
+        nextPosition = ball.s + ball.v * PhisicalEntity.dt
+        if nextPosition[0] >= DISPLAY_WIDTH - 2 * ball.r:
+            ball.s[0] = DISPLAY_WIDTH - 2 * ball.r
+
+        impactAngle = np.arctan2(ball.v[1], ball.v[0])
+        # print(f"Impact angle: {np.rad2deg(impactAngle)}, Out angle should be {np.rad2deg(np.pi - impactAngle)}")
+        vx, vy = rotate(ball.v, np.pi + 2 * impactAngle)
+        # print("floor {} {}, old {}".format(vx, vy, ball.v))
+        ball.v[0], ball.v[1] = vx, vy
+
+        outAngle = np.arctan2(vy, vx)
+        # print(f"Out angle is {np.rad2deg(outAngle)}")
+
+    dist = np.linalg.norm((slime1.s+slime1.r) - (ball.s+ball.r), axis=0)  # slime1 <-> ball euclidian distance
+    #dist = np.linalg.norm()
+    if dist <= slime1.r + ball.r + 0.5:
+        #d = slime1.s[1]-ball.s[1]-2*ball.r # distance from touching point to the floor
+        # r = slime1.r
+        # y_dist = slime1.s[1] + slime1.r - ball.s[1] - ball.r
+        # y_dist = np.abs(y_diff)
+        # x_dist = slime1.s[0] + slime1.r - ball.s[0] - ball.r
+        # x_dist = np.abs(x_diff)
+
+        impactAngleBall = np.arctan2(ball.v[1], ball.v[0])
+        impactAngleSlime = np.arctan2(slime1.v[1], slime1.v[0])
+
+        centerImpactDistance = ball.s - slime1.s
+        normalAngle = np.arctan2(centerImpactDistance[1], centerImpactDistance[0])
+
+        tangentAngle = normalAngle - np.pi / 2
+
+        ballReflectionOfSlimeAngle = tangentAngle + impactAngleSlime - np.pi / 2
+        slimeImpactVelocity = slime1.v
+
+        #beta = np.pi/2 - alpha
+        x, y = rotate(ball.v, np.pi / 2 - tangentAngle)
+
+        ball.v[0], ball.v[1] = x + slimeImpactVelocity[0], y + slimeImpactVelocity[1]
+        ball.v[0], ball.v[1] = 0.9 * ball.v[0], 0.9 * ball.v[1]
+
     # Collision detection end.
 
     for entity in entities:
